9a754717096dad1ee4a7db8bb19c7fb591506f52
Peter Rossbach <peterrossbach@me.com>
2013-11-04 17:27:22 +0100

    Setup Background Timer Events

---
+++ timer/Timer.java
@@ -0,0 +1,27 @@
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.platform.Verticle;
+import org.vertx.java.core.logging.Logger;
+
+public class Timer extends Verticle {
+
+	public void start() {
+		Logger logger = container.logger();
+      	
+		long timerID = vertx.setPeriodic(1000, new Handler<Long>() {
+		    int count;
+		     
+		    public void handle(Long timerID) {  
+		        System.out.println("In event handler " + count + " " + System.nanoTime() + "\n"); 
+		        if (++count == 10) {
+		            vertx.cancelTimer(timerID);
+		        }          
+		    }
+		});
+		logger.info("sleep start\n");
+		try { 
+			Thread.sleep(10000);
+		} catch (Exception ex) {};
+		logger.info("sleep stop\n");
+	}
+}
