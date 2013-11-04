e5194061f8a07f726d60a5de52ba40b4be2cb34e
Peter Rossbach <peterrossbach@me.com>
2013-11-04 17:26:59 +0100

    Simple Hello WJAX HTTP Server

---
+++ hello/HelloServer.java
@@ -0,0 +1,22 @@
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.platform.Verticle;
+import org.vertx.java.core.logging.Logger;
+
+import java.util.Map;
+
+public class HelloServer extends Verticle {
+
+  public void start() {
+    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+      public void handle(HttpServerRequest req) {      	
+      	Logger logger = container.logger();
+      	logger.info(Thread.currentThread().getId());
+      	
+        req.response().headers().set("Content-Type", "text/plain");
+        req.response().end("Hello Wjax\n");
+      }
+    }).listen(8080);
+  }
+}
