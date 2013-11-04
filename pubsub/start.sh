5aa7005b1bb6a3d20e42ac088aff44e03284617c
Peter Rossbach <peterrossbach@me.com>
2013-11-04 19:11:01 +0100

    add JMX Opts

--- pubsub/start.sh
+++ pubsub/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export VERTX_OPTS="-Dhazelcast.version.check.enabled=false"
+VERTX_OPTS="-Dhazelcast.version.check.enabled=false"
+VERTX_OPTS="$VERTX_OPTS -Dcom.sun.management.jmxremote -Dvertx.management.jmx=true -Dhazelcast.jmx=true"
+export VERTX_OPTS
 
 mkdir log
 nohup bash -c "exec 'vertx' 'run' 'pubsub_server.js' -conf conf/conf1.json -cluster &> log/s1.log" &> /dev/null &
