6cc1dc8dc8e05948d70fc1fb01c727a09233831d
Peter Rossbach <peterrossbach@me.com>
2013-11-04 17:28:44 +0100

    create log dir

--- pubsub/start.sh
+++ pubsub/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export VERTX_OPTS="-Dhazelcast.version.check.enabled=false"
 
+mkdir log
 nohup bash -c "exec 'vertx' 'run' 'pubsub_server.js' -conf conf/conf1.json -cluster &> log/s1.log" &> /dev/null &
  
 nohup bash -c "exec 'vertx' 'run' 'pubsub_server.js' -conf conf/conf2.json -cluster &> log/s2.log" &> /dev/null &
